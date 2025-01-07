8fb7762216339097d4fada3149cdfd38480f1b04
Badr Abdallaoui <badredda@amazon.com>
2025-01-07 17:54:48 +0100

    style change

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,31 @@
-//document.getElementById('order-form').addEventListener('submit', function (e) {
-//    e.preventDefault();
-//
-//    const form = e.target;
-//    const formData = new FormData(form);
-//
-//    // Convert FormData to a plain object
-//    const data = {};
-//    formData.forEach((value, key) => {
-//        data[key] = value;
-//    });
-//
-//    // Replace with your Google Apps Script URL
-//    const scriptURL = 'https://script.google.com/macros/s/AKfycbyvMLwVpdz_xV3AlS2QJk09ssnfDKRAq9QPW19J7TtT1-sn2fl2r__Awbdbtcc2PzTa/exec';
-//
-//    fetch(scriptURL, {
-//        method: 'POST',
-//        body: JSON.stringify(data), // Send data as JSON
-//        headers: {
-//            'Content-Type': 'application/json' // Set content type to JSON
-//        }
-//    })
-//    .then(response => response.json()) // Parse the JSON response
-//    .then(result => {
-//        // Show success message
-//        document.getElementById('success-message').style.display = 'block';
-//        form.reset(); // Reset the form
-//    })
-//    .catch(error => {
-//        console.error('Error:', error);
-//        alert('حدث خطأ أثناء إرسال الطلب. يرجى المحاولة مرة أخرى.');
-//    });
-//});
+function submitForm() {
+    const form = document.getElementById("orderForm");
+    const formData = {
+        name: form.name.value,
+        phone: form.phone.value,
+        address: form.address.value,
+        quantity: form.quantity.value,
+    };
+
+    fetch("https://script.google.com/macros/s/AKfycbxc9l5mJYamyxxVg52-3o15v27woguKNKy0a_GtisbOk7lhUhAYUDSCLkAcyxCNL5yU/exec", {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.status === "success") {
+                alert("تم إرسال الطلب بنجاح!");
+                form.reset();
+            } else {
+                alert("حدث خطأ أثناء إرسال الطلب.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            alert("حدث خطأ أثناء إرسال الطلب.");
+        });
+}
+
